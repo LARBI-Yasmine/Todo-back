123458f1f5caed2b7c07c4cc90af13a774feeb43
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-06-16 20:11:42 +0200

    add tache Model

---
+++ src/models/tache.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const TacheSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  date: { type: Date, required: true },
+  completed: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("tache", TacheSchema);
